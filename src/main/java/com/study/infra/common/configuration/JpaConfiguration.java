d4f7a9ec07dfd34cd2d57c2486320f4a597bb38c
omega <soh95@naver.com>
2025-02-20 18:05:21 +0900

    feat: add jpa config

---
+++ src/main/java/com/study/infra/common/configuration/JpaConfiguration.java
@@ -0,0 +1,10 @@
+package com.study.infra.common.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration(proxyBeanMethods = false)
+public class JpaConfiguration {
+
+}
