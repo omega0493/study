997a8665840c9250f542866a5d8a447ef442c029
omega <soh95@naver.com>
2025-02-22 12:25:35 +0900

    feat: add UserModelArgumentResolver

---
+++ src/main/java/com/study/infra/security/UserModelArgumentResolver.java
@@ -0,0 +1,39 @@
+package com.study.infra.security;
+
+import com.study.api.auth.model.UserModel;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class UserModelArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return UserModel.class.isAssignableFrom(parameter.getParameterType());
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) throws Exception {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            return null;
+        }
+
+        Object details = auth.getDetails();
+        if (details instanceof UserModel userModel) {
+            return userModel;
+        }
+
+        return null;
+    }
+
+}
