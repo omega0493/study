70cbabec42e9d865f7a9da4b0f9a8b242f361ed7
omega <soh95@naver.com>
2025-03-10 18:08:09 +0900

    feat: add BoardRepositoryTest

---
+++ src/test/java/com/study/api/board/repository/BoardRepositoryTest.java
@@ -0,0 +1,102 @@
+package com.study.api.board.repository;
+
+import com.study.api.auth.constant.UserRole;
+import com.study.api.auth.model.UserModel;
+import com.study.api.auth.repository.UserRepository;
+import com.study.api.board.model.BoardModel;
+import com.study.entity.board.Board;
+import com.study.entity.user.User;
+import com.study.infra.common.configuration.JpaConfiguration;
+import com.study.test.config.TestAuditorAwareImpl;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+@Import({JpaConfiguration.class, TestAuditorAwareImpl.class})
+@DataJpaTest
+class BoardRepositoryTest {
+
+    private static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:latest")
+            .withUsername("admin")
+            .withPassword("foobar")
+            .withDatabaseName("study");
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", mySQLContainer::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", mySQLContainer::getPassword);
+        dynamicPropertyRegistry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        mySQLContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mySQLContainer.stop();
+    }
+
+    @Test
+    void findAllWithUserTest() {
+
+        //given
+        User user = User.builder()
+                .userName("foo")
+                .userPassword("$2a$10$u6g7CRVI8PAZa7sz7xNkjOi2F6Jwpf8d08vAX5W0eX8T.RxUhVFy2")
+                .userRole(UserRole.USER)
+                .build();
+
+        entityManager.persist(user);
+
+        Board board = Board.builder()
+                .title("Spring Security 설정하기")
+                .content("Spring Security를 활용한 인증과 인가 설정 방법을 정리합니다.")
+                .user(user)
+                .createDate(null)
+                .updateDate(null)
+                .build();
+
+        List<Board> boardList = new ArrayList<>();
+        boardList.add(board);
+
+        for (Board board1 : boardList) {
+            entityManager.persist(board1);
+        }
+
+        //when
+        List<Board> boards = boardRepository.findAllWithUser();
+
+        //then
+        assertThat(boards)
+                .isNotNull()
+                .hasSize(1)  // 리스트에 하나의 Board 객체가 있는지 확인
+                .containsExactly(boardList.get(0));  // boardList의 첫 번째 Board 객체와 동일한지 확인
+    }
+}
