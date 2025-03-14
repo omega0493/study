4c2132b80c48cc9020b246467ab63044e33cb90c
omega <soh95@naver.com>
2025-03-14 17:07:29 +0900

    feat: add AuditorAwareImplTest

---
+++ src/test/java/com/study/infra/security/AuditorAwareImplTest.java
@@ -0,0 +1,74 @@
+package com.study.infra.security;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AuditorAwareImplTest {
+
+    private AuditorAwareImpl sut;
+
+    @Mock
+    private Authentication authentication;
+
+    @BeforeEach
+    void setUp() {
+        sut = new AuditorAwareImpl();
+    }
+
+    @Test
+    void getCurrentAuditorTest() {
+
+        // given
+        when(authentication.isAuthenticated()).thenReturn(true);
+        when(authentication.getName()).thenReturn("foo");
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // when
+        Optional<String> maybeCurrentAuditor = sut.getCurrentAuditor();
+
+        // then
+        assertThat(maybeCurrentAuditor)
+                .isNotNull()
+                .isPresent()
+                .hasValue("foo");
+    }
+
+    @Test
+    void fail1() {
+        // when
+        Optional<String> maybeCurrentAuditor = sut.getCurrentAuditor();
+
+        // then
+        assertThat(maybeCurrentAuditor)
+                .isNotNull()
+                .isEmpty();
+    }
+
+    @Test
+    void fail2() {
+
+        // given
+        when(authentication.isAuthenticated()).thenReturn(false);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // when
+        Optional<String> maybeCurrentAuditor = sut.getCurrentAuditor();
+
+        // then
+        assertThat(maybeCurrentAuditor)
+                .isNotNull()
+                .isEmpty();
+    }
+
+}
