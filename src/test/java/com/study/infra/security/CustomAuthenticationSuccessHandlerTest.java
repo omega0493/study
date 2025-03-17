fc1be5444a9d4e126ea8ab32612376bf01cfd4df
omega <soh95@naver.com>
2025-03-17 18:29:29 +0900

    test: modify CustomAuthenticationSuccessHandlerTest

--- src/test/java/com/study/infra/security/CustomAuthenticationSuccessHandlerTest.java
+++ src/test/java/com/study/infra/security/CustomAuthenticationSuccessHandlerTest.java
@@ -2,39 +2,36 @@ package com.study.infra.security;
 
 import com.study.api.auth.constant.UserRole;
 import com.study.api.auth.model.UserModel;
-import com.study.api.auth.repository.UserRepository;
 import com.study.api.auth.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = CustomAuthenticationSuccessHandler.class)
 class CustomAuthenticationSuccessHandlerTest {
 
-    @InjectMocks
+    @Autowired
     private CustomAuthenticationSuccessHandler successHandler;
 
-    @Mock
+    @MockitoBean
     private AuthService authService;
 
     @Mock
     private Authentication authentication;
-
-    @Mock
-    private UserRepository userRepository;
 
     @Mock
     private HttpServletRequest request;
@@ -59,22 +56,16 @@ class CustomAuthenticationSuccessHandlerTest {
         // when
         successHandler.onAuthenticationSuccess(request, response, authentication);
 
+        // then
         Authentication contextAuth = SecurityContextHolder.getContext().getAuthentication();
 
-        // then
-        assertThat(contextAuth).isNotNull();
-
-        assertThat(contextAuth.getAuthorities().stream().map(GrantedAuthority::getAuthority))
-                .containsExactly(String.valueOf(new SimpleGrantedAuthority(UserRole.USER.getCode())));
-
-        assertThat(contextAuth.getPrincipal())
-                .isEqualTo("foo");
-
-        assertThat(contextAuth.getCredentials())
-                .isEqualTo("bar");
-
-        assertThat(contextAuth.getDetails())
-                .isEqualTo(model);
-
+        assertThat(contextAuth)
+                .isNotNull()
+                .returns("foo", Authentication::getPrincipal)
+                .returns("bar", Authentication::getCredentials)
+                .returns(model, Authentication::getDetails)
+                .extracting(Authentication::getAuthorities, as(InstanceOfAssertFactories.LIST))
+                .containsExactly(new SimpleGrantedAuthority(UserRole.USER.getCode()));
     }
+
 }
